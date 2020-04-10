5225eeed45f117f20362e7e99b471a545d9a8aa0
patricke25 <sylvain.pegon2@laposte.net>
2020-04-10 16:40:39 +0200

    ajout codenarc.xml

--- src/main/groovy/gl51/MovieController.groovy
+++ src/main/groovy/gl51/MovieController.groovy
@@ -3,7 +3,6 @@ package gl51
 import gl51.movie.data.MovieRequest
 import gl51.movie.data.Movie
 import gl51.movie.service.MovieRegistry
-import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.HttpStatus
